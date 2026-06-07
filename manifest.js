c2961f3ce6664a1dfe3994b10a0f6f9ebde0c3b5
Juan Manuel <juanmanuel1994u@gmail.com>
2026-06-07 16:22:30 -0300

    Add files via upload

---
+++ manifest.js
@@ -0,0 +1,12 @@
+(function () {
+  "use strict";
+  window.__BRAND__ = {
+    name: "Your Business Name",
+    tagline: "Modern solutions for growing businesses.",
+    contact: {
+      email: "your@email.com",
+      phone: "+1 (000) 000-0000",
+      address: "Your City, Country"
+    }
+  };
+})();
